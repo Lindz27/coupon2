8225eb112db9004cfcca5a100a107148a46f9aec
Lindsey Davis <lindseynd2011@gmail.com>
2016-03-08 20:56:43 -0600

    replaced chat with coupon everywhere and deleted view page for individual pages; deleted references for images

--- www/js/app.js
+++ www/js/app.js
@@ -50,21 +50,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
+  .state('tab.coupons', {
+      url: '/coupons',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+        'tab-coupons': {
+          templateUrl: 'templates/tab-coupons.html',
+          controller: 'CouponsCtrl'
         }
       }
     })
--- www/js/services.js
+++ www/js/services.js
@@ -1,47 +1,52 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
+.factory('Coupons', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var chats = [{
+  var coupons = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
+    name: 'Kroger',
+    dueDate: new Date(),
+    discountProduct: 'almonds',
+    discountAmount:"$2",
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    name: "Macy's",
+    dueDate: new Date(),
+    discountProduct: 'jeans',
+    discountAmount:"50%",
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
+    name: 'Express',
+    dueDate: new Date(),
+    discountProduct: 'shorts',
+    discountAmount:"$2",
   }, {
     id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
+    name: 'Whole Foods',
+    dueDate: new Date(),
+    discountProduct: 'cookies',
+    discountAmount:"$2",
   }, {
     id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    name: 'Guitar Center',
+    dueDate: new Date(),
+    discountProduct: 'guitar picks',
+    discountAmount:"$10",
   }];
 
   return {
     all: function() {
-      return chats;
+      return coupons;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
+    remove: function(coupon) {
+      coupons.splice(coupons.indexOf(coupon), 1);
     },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    get: function(couponId) {
+      for (var i = 0; i < coupons.length; i++) {
+        if (coupons[i].id === parseInt(couponId)) {
+          return coupons[i];
         }
       }
       return null;
