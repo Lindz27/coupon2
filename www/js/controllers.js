3c97bf2732e9d088b30be8cda5b8e16994c3cd61
Lindsey Davis <lindseynd2011@gmail.com>
2016-03-22 20:55:54 -0500

    added local storage inf into controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,6 +12,59 @@ angular.module('starter.controllers', ['ui.rCalendar'])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
+
+angular.module('ionic.utils', [])
+
+.factory('$localstorage', ['$window', function($window) {
+  return {
+    set: function(key, value) {
+      $window.localStorage[key] = value;
+    },
+    get: function(key, defaultValue) {
+      return $window.localStorage[key] || defaultValue;
+    },
+    setObject: function(key, value) {
+      $window.localStorage[key] = JSON.stringify(value);
+    },
+    getObject: function(key) {
+      return JSON.parse($window.localStorage[key] || '{}');
+    }
+  }
+}]);
+//Using window.localStorage directly is just fine, but having to set and parse Strings gets tiresome after a while. Use this simple AngularJS service for setting and retrieving strings or objects easily:
+
+angular.module('app', ['ionic', 'ionic.utils'])
+
+.run(function($localstorage) {
+
+  $localstorage.set('name', 'Max');
+  console.log($localstorage.get('name'));
+  $localstorage.setObject('post', {
+    name: 'Thoughts',
+    text: 'Today was a good day'
+  });
+
+  var post = $localstorage.getObject('post');
+  console.log(post);
+});
+//And to use this service, just inject the $localstorage service into a controller or run function:
+
+angular.module('app', ['ionic', 'ionic.utils'])
+
+.run(function($localstorage) {
+
+  $localstorage.set('name', 'Max');
+  console.log($localstorage.get('name'));
+  $localstorage.setObject('post', {
+    name: 'Thoughts',
+    text: 'Today was a good day'
+  });
+
+  var post = $localstorage.getObject('post');
+  console.log(post);
+});
+
+
   $scope.coupons = Coupons.all();
   $scope.remove = function(coupon) {
     Coupons.remove(coupon);
